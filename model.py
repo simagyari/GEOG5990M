5e0b6d01f10ae1dc89a9cb7f61b275b7336aec9d
simagyari <magyari.sandor.istvan@gmail.com>
2022-01-24 20:00:36 +0000

    Individual coordinates changed to list elements

--- model.py
+++ model.py
@@ -1,65 +1,72 @@
 # Import statements
 import random
 
-# Assigning variables
-y0 = 50
-x0 = 50
 
-y1 = 50
-x1 = 50
+agents = []
+
+# Assigning variables
+y0 = random.randint(0, 99)
+x0 = random.randint(0, 99)
+agents.append([y0, x0])
+
+y1 = random.randint(0, 99)
+x1 = random.randint(0, 99)
+agents.append([y1, x1])
+
+print(agents)
 
 # Random movement for y0 and x0
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
 
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
+    agents[0][1] -= 1
 
-print(y0, x0)
+print(agents[0][0], agents[0][1])
 
 # Second step for 0s
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
 
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
+    agents[0][1] -= 1
 
-print(y0, x0)
+print(agents[0][0], agents[0][1])
 
 # Random movement for y1 and x1
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
 
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
-print(y1, x1)
+print(agents[1][0], agents[1][1])
 
 # Second step for 1s
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
 
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
+    agents[1][1] -= 1
 
-print(y1, x1)
+print(agents[1][0], agents[1][1])
 
 # Calculate Euclidean distance between points
-distance = ((y0 - y1)**2 + (x0 - x1)**2)**0.5
+distance = ((agents[0][0] - agents[1][0])**2 + (agents[0][1] - agents[1][1])**2)**0.5
 print("Distance of the points is", distance)

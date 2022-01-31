0adf477cb590a5f9d747606a47a8220f15fb2c6d
simagyari <magyari.sandor.istvan@gmail.com>
2022-01-31 17:17:21 +0000

    Setting random seed, torus space constraint, optimized distance calculation

--- model.py
+++ model.py
@@ -4,12 +4,16 @@ import operator
 import matplotlib.pyplot as plt
 import time
 
+# Setting the seed of the random module
+random.seed(42)
+
 # Define function for pythagorean distance
 def distance_between(agents_row_a, agents_row_b):
     return ((agents_row_a[0] - agents_row_b[0])**2 + (agents_row_a[1] - agents_row_b[1])**2)**0.5
 
+
 # Add number of agents
-num_of_agents = 100
+num_of_agents = 10
 num_of_iterations = 100
 
 # Initialising empty agent coordinates list
@@ -21,33 +25,34 @@ for i in range(num_of_agents):
 
 print(agents)
 
+# Space size constraint
+space = 100
 # Random movement for agent coordinates
 # Loop through every movement instance
 for i in range(num_of_iterations):
     # Loop through every agent in the agents list
     for k in range(num_of_agents):
         # Create random movement for y coordinate
-        # Implementing Torus space by adding modulo of 100, agents placed tp other side if wandering off
+        # Implementing Torus space by adding modulo of the space constraint, agents placed to other side if wandering off
         if random.random() < 0.5:
-            agents[k][0] = (agents[k][0] + 1) % 100
+            agents[k][0] = (agents[k][0] + 1) % space
         else:
-            agents[k][0] = (agents[k][0] - 1) % 100
+            agents[k][0] = (agents[k][0] - 1) % space
         # Create random movement for x coordinate
         if random.random() < 0.5:
-            agents[k][1] = (agents[k][1] + 1) % 100
+            agents[k][1] = (agents[k][1] + 1) % space
         else:
-            agents[k][1] = (agents[k][1] - 1) % 100
+            agents[k][1] = (agents[k][1] - 1) % space
 
 # # Calculate Euclidean distance between points
 start = time.process_time()  # Timing of distance starts
 for i in range(len(agents)):
-    for j in range(len(agents)):
+    for j in range(i+1, len(agents), 1):
         print('Distance between agents', i, 'and', j, 'is:', distance_between(agents[i], agents[j]))
 end = time.process_time()  # Timing of distance ends
 print('Time of distance function:', str(end - start))
 # distance = distance_between(agents[0], agents[1])
 # print(distance)
-
 
 # Print the maximum of the two lists by the first element
 print('The maximum of elements by y coordinates:', max(agents))

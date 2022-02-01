5da545d859aada98f8324e844116feb4a5438daa
simagyari <magyari.sandor.istvan@gmail.com>
2022-02-01 14:23:32 +0000

    Move method implemented

--- agentframework.py
+++ agentframework.py
@@ -6,3 +6,14 @@ class Agent:
     def __init__(self):
         self.x = random.randint(0, 99)
         self.y = random.randint(0, 99)
+
+    def move(self):
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 100
+        else:
+            self.y = (self.y - 1) % 100
+
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+        else:
+            self.x = (self.x - 1) % 100

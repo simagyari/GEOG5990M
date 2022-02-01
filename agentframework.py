dce18b989a0db98ce00378c43d2b2cf7d1cdf688
simagyari <magyari.sandor.istvan@gmail.com>
2022-02-01 14:46:17 +0000

    ADD: Name mangling and property added to x and y of agent

--- agentframework.py
+++ agentframework.py
@@ -4,16 +4,31 @@ import random
 class Agent:
 
     def __init__(self):
-        self.x = random.randint(0, 99)
-        self.y = random.randint(0, 99)
+        self.__x = random.randint(0, 99)
+        self.__y = random.randint(0, 99)
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def set_x(self, value):
+        self.__x = value
+
+    def set_y(self, value):
+        self.__y = value
+
+    x = property(get_x, set_x, "I'm the 'x' property!")
+    y = property(get_y, set_y, "I'm the 'y' property!")
 
     def move(self):
         if random.random() < 0.5:
-            self.y = (self.y + 1) % 100
+            self.__y = (self.__y + 1) % 100
         else:
-            self.y = (self.y - 1) % 100
+            self.__y = (self.__y - 1) % 100
 
         if random.random() < 0.5:
-            self.x = (self.x + 1) % 100
+            self.__x = (self.__x + 1) % 100
         else:
-            self.x = (self.x - 1) % 100
+            self.__x = (self.__x - 1) % 100

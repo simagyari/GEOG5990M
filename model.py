0cfa785e2bc8f880bec278f90332c7d840268850
simagyari <magyari.sandor.istvan@gmail.com>
2022-02-01 16:34:40 +0000

    ADD: reading environmental raster from csv file

--- model.py
+++ model.py
@@ -1,10 +1,27 @@
-import matplotlib.pyplot
+from msilib.schema import Environment
+import matplotlib.pyplot as plt
 import agentframework
+import csv
 
 # Measures distance between agents
 def distance_between(agents_row_a, agents_row_b):
     return (((agents_row_a.x - agents_row_b.x)**2) +
         ((agents_row_a.y - agents_row_b.y)**2))**0.5
+
+
+# Reading raster data
+with open('in.txt', 'r') as f:
+    environment = []
+    reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)  # QUOTE_NONNUMERIC changes everything to float
+    for row in reader:
+        rowlist = []
+        for value in row:
+            rowlist.append(value)
+        environment.append(rowlist)
+
+# Check if environment raster is correctly represented
+plt.imshow(environment)
+plt.show()
 
 # Initialise single agent
 a = agentframework.Agent()
@@ -28,11 +45,11 @@ for j in range(num_of_iterations):
         agents[i].move()
 
 # Plot agents on a scatterplot recursively adding points
-matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.ylim(0, 99)
+plt.xlim(0, 99)
+plt.ylim(0, 99)
 for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i].x, agents[i].y)
-matplotlib.pyplot.show()
+    plt.scatter(agents[i].x, agents[i].y)
+plt.show()
 
 # Recursively measure distances
 for agents_row_a in agents:

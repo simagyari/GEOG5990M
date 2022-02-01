358e3c0112fb76e3bfcbe0334c82391f54b08b37
simagyari <magyari.sandor.istvan@gmail.com>
2022-02-01 12:21:41 +0000

    Code rewritten to accept OOP agents, one test agent initialised

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
 import random
 import operator
 import matplotlib.pyplot
+import agentframework
 
 # Measures distance between agents
 def distance_between(agents_row_a, agents_row_b):
-    return (((agents_row_a[0] - agents_row_b[0])**2) +
-        ((agents_row_a[1] - agents_row_b[1])**2))**0.5
+    return (((agents_row_a.x - agents_row_b.x)**2) +
+        ((agents_row_a.y - agents_row_b.y)**2))**0.5
+
+# Initialise single agent
+a = agentframework.Agent()
+type(a)  # Check if it is an agentframework agent
 
 # Declare number of agents and iterations
 num_of_agents = 10
@@ -14,27 +19,19 @@ agents = []  # Initialise list of agents
 
 # Make the agents.
 for i in range(num_of_agents):
-    agents.append([random.randint(0,99),random.randint(0,99)])
+    agents.append(agentframework.Agent())
 
 # Move the agents.
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
 
-        if random.random() < 0.5:
-            agents[i][0] = (agents[i][0] + 1) % 100
-        else:
-            agents[i][0] = (agents[i][0] - 1) % 100
-
-        if random.random() < 0.5:
-            agents[i][1] = (agents[i][1] + 1) % 100
-        else:
-            agents[i][1] = (agents[i][1] - 1) % 100
+        agents[i].move()
 
 # Plot agents on a scatterplot recursively adding points
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
 for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+    matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
 matplotlib.pyplot.show()
 
 # Recursively measure distances

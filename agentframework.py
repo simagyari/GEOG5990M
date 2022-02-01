a7f30f100628bfd3c3b5092be699b6c20223c4c3
simagyari <magyari.sandor.istvan@gmail.com>
2022-02-01 16:47:50 +0000

    ADD: eat method for agents, inline comments added

--- agentframework.py
+++ agentframework.py
@@ -1,13 +1,17 @@
 import random
 
 
+# Defines Agent class, containing agents for abm
 class Agent:
 
+    # Instance variables of the class objects
     def __init__(self, environment):
         self.__x = random.randint(0, 99)
         self.__y = random.randint(0, 99)
         self.environment = environment
+        self.store = 0
 
+    # Getter and setter functions for name-mangled variables
     def get_x(self):
         return self.__x
 
@@ -20,16 +24,24 @@ class Agent:
     def set_y(self, value):
         self.__y = value
 
+    # Property values for name-mangled variables
     x = property(get_x, set_x, "I'm the 'x' property!")
     y = property(get_y, set_y, "I'm the 'y' property!")
 
+    # Moves agent (y and x coordinates respectively) in a Torus space of 100x100 units
     def move(self):
         if random.random() < 0.5:
-            self.__y = (self.__y + 1) % 100
+            self.y = (self.y + 1) % 100
         else:
-            self.__y = (self.__y - 1) % 100
+            self.y = (self.y - 1) % 100
 
         if random.random() < 0.5:
-            self.__x = (self.__x + 1) % 100
+            self.x = (self.x + 1) % 100
         else:
-            self.__x = (self.__x - 1) % 100
+            self.x = (self.x - 1) % 100
+
+    def eat(self): # can you make it eat what is left?
+        # If the cell has more food than 10, eats 10 of it (subtract and store)
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 10

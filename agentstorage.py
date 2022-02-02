568386de7f85c46cbf55dc052788996b673dec0b
simagyari <magyari.sandor.istvan@gmail.com>
2022-02-02 16:26:56 +0000

    ADD: agentstorage file created, writes overall and agentwise storage to storage.txt file

---
+++ agentstorage.py
@@ -0,0 +1,19 @@
+# Sums stored food of every agent in the list
+def all_storage_writer(agents):
+    all_storage = 0
+    for agent in agents:
+        all_storage += agent.store
+    print('Storage is', all_storage)  # Prints current run's storage on terminal
+    # Appends storage to storage.txt file
+    with open('storage.txt', 'a') as f:
+        f.write('Combined storage of all agents: ' + str(all_storage) + '\n')
+    print('Total amount stored by all agents recorded in "storage.txt"')
+
+def agent_storage_writer(agents):
+    agent_storage = []
+    for agent in agents:
+        agent_storage.append(agent.store)
+    print('Storage of agents:\n', agent_storage)
+    with open('storage.txt', 'a') as f:
+        f.write('Individual storage of agents: ' + str(agent_storage)[1:-1] + '\n')
+    print('Amount of food stored by each agent recorded in "storage.txt"')
